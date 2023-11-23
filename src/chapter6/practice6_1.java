93010583f0604906d1f508bbcc6c9007454f6195
kkite <kkite6789@naver.com>
2023-11-23 22:44:01 +0900

    6-1 선택정렬(Selection sort)복습 - 풀이 완료

---
+++ src/chapter6/practice6_1.java
@@ -0,0 +1,44 @@
+package chapter6;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*
+1. 선택 정렬
+설명
+N개의 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
+정렬하는 방법은 선택정렬입니다.
+
+입력
+첫 번째 줄에 자연수 N(1<=N<=100)이 주어집니다.
+두 번째 줄에 N개의 자연수가 공백을 사이에 두고 입력됩니다. 각 자연수는 정수형 범위 안에 있습니다.
+
+출력
+오름차순으로 정렬된 수열을 출력합니다.
+ */
+public class practice6_1 {
+
+    public static void solution(int n, int[] arr){
+        int swap;
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[i]>arr[j]){
+                    swap=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=swap;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        solution(n,arr);
+    }
+}
