f943e6602fe329815e719885b0388c8a5f47b2de
kkite <kkite6789@naver.com>
2023-12-19 23:53:28 +0900

    7-14 최단거리2(BFS) - 풀이 완료

---
+++ src/chapter7/practice7_14.java
@@ -0,0 +1,27 @@
+package chapter7;
+
+public class practice7_14 {
+    static int n;
+    static int[] ch;
+    public static void DFS(int L){
+        if(L==n+1){
+            StringBuilder tmp= new StringBuilder();
+            for(int i=1; i<=n; i++){
+                if(ch[i]==1) tmp.append(i).append(" ");
+            }
+            if(tmp.length()>0) System.out.println(tmp);
+        }
+        else{
+            ch[L]=1;
+            DFS(L+1);
+            ch[L]=0;
+            DFS(L+1);
+        }
+    }
+
+    public static void main(String[] args){
+        n=3;
+        ch=new int[n+1];
+        DFS(1);
+    }
+}
